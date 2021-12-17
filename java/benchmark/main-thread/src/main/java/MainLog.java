7bc9a789911d769f26626c92aa6a842f34f79f32
tam888888 <trinhvan.tam.group.8@gmail.com>
2021-12-17 02:08:25 +0000

    add

--- java/benchmark/main-thread/src/main/java/MainLog.java
+++ java/benchmark/main-thread/src/main/java/MainLog.java
@@ -4,29 +4,27 @@ import org.apache.logging.log4j.core.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 import java.io.File;
 
-public class MainLog
-{
- public static void main(String[] args) {
-    System.out.println("ABC");
+public class MainLog {
+    public static void main(String[] args) {
+        System.out.println("ABC");
 
         String log4jFile = System.getProperty("log4j.configurationFile");
-        if (log4jFile == null || ! (new File(log4jFile)).exists()) {
-        LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
-        File log4j = new File("./log4j2.xml");
-        ctx.setConfigLocation (log4j.toURI());
+        if (log4jFile == null || !(new File(log4jFile)).exists()) {
+            LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+            File log4j = new File("./log4j2.xml");
+            ctx.setConfigLocation(log4j.toURI());
         }
 
-    MetricsBenchmark mb = new MetricsBenchmark();
+        MetricsBenchmark mb = new MetricsBenchmark();
 
-    while(true){
-        long t1 = System.nanoTime();
+        while (true) {
+            long t1 = System.nanoTime();
 
-        LockSupport.parkNanos(1000000);
-        // System.out.println("Log test");
-        mb.statisticMetris(t1, 1024, "Test");
+            LockSupport.parkNanos(1000000);
+            // System.out.println("Log test");
+            mb.statisticMetris(t1, 1024, "Test");
 
-
+        }
     }
-}
 
 }
