0897b96edf0806fca8a7bf8d8cf3ffae73b1ab98
tam888888 <trinhvan.tam.group.8@gmail.com>
2022-08-25 09:44:41 +0000

    add

--- name/point2.js
+++ name/point2.js
@@ -19,11 +19,11 @@ log4js.configure({
   // const browser = await puppeteer.launch();
   // const page = await browser.newPage();
   // page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
-  var myArgs = process.argv.slice(2);  
+  var myArgs = process.argv.slice(2);
   console.log(myArgs);
   var names = [];
   for (let f of myArgs) {
-    const data = fs.readFileSync(f, { encoding: 'utf8', flag: 'r' });    
+    const data = fs.readFileSync(f, { encoding: 'utf8', flag: 'r' });
     var nn = data.split("\n");
     for (let n of nn) names.push(n);
   }
@@ -33,31 +33,34 @@ log4js.configure({
 
   for (let x of names) {
     console.log("====================== Name " + x + " ======================");
-    x = x.replaceAll("\"","");
+    x = x.replaceAll("\"", "");
     var p = 0;
     while (true) {
       var url = urlp1 + x + urlp2;
       console.log(url);
       try {
-        const t = await fetch(url,  { timeout: 5000 });
+        const t = await fetch(url, { timeout: 5000 });
         var z = await t.text();
         var key = "lb_danhgia\">";
         var idx1 = z.indexOf(key) + key.length;
-        var idx2 = z.indexOf("</span>",idx1);
-        var pointStr = z.substring(idx1,idx2);
+        var idx2 = z.indexOf("</span>", idx1);
+        var pointStr = z.substring(idx1, idx2);
         var idx = pointStr.indexOf("/");
-        for(var i = idx; i >=0 ; i--){
-          if(pointStr.charAt(i) == ' '){
-            console.log(pointStr.substring(i+1,idx) + " " + pointStr.substring(0,i));
+        for (var i = idx; i >= 0; i--) {
+          if (pointStr.charAt(i) == ' ') {
+            console.log(pointStr.substring(i + 1, idx) + " " + pointStr.substring(0, i));
+            logger.info(pointStr.substring(i + 1, idx) + " " + pointStr.substring(0, i));
             break;
           }
         }
         // console.log(z.substring(idx1,idx2));
-        break;        
+        break;
       } catch (e) {
         console.log(e);
       }
     }
   }
-
+  for (let f of myArgs) {
+    console.log("====================== END " + f + " ======================");
+  }
 })();
