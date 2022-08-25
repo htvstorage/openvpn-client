2f2cf1e1c78ce97a51283e2e5a5f02a2337abbad
tam888888 <trinhvan.tam.group.8@gmail.com>
2022-08-25 09:26:06 +0000

    add

--- name/point.js
+++ name/point.js
@@ -19,72 +19,62 @@ log4js.configure({
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
-  var myArgs = process.argv.slice(2);
-  // var stories=myArgs[0];
-
+  var myArgs = process.argv.slice(2);  
   console.log(myArgs);
   var names = [];
-  for(let f of myArgs){
-    const data = fs.readFileSync(f, {encoding:'utf8', flag:'r'});
-    // console.log(data.substring(1,100));
+  for (let f of myArgs) {
+    const data = fs.readFileSync(f, { encoding: 'utf8', flag: 'r' });    
     var nn = data.split("\n");
-    for(let n of nn) names.push(n);
+    for (let n of nn) names.push(n);
   }
 
-  // var abc = stories.split(",");
   var urlp1 = "https://tenchocon.vn/name/Tr%E1%BB%8Bnh-Tr%C3%BAc-Nh%C3%A3-";
   var urlp2 = ".html";
 
-  for(let x of names){  
-    console.log("====================== Name " + x +" ======================");
+  for (let x of names) {
+    console.log("====================== Name " + x + " ======================");
+    x = x.replaceAll("\"","");
     var p = 0;
-    while(true){
+    while (true) {
       var url = urlp1 + x + urlp2;
       console.log(url);
       // await page.setDefaultNavigationTimeout(0);
-      try{
-      await page.goto(url);
-      await page.screenshot({ path: 'images/name_'+x+'.png' });
-      const ar = await page.evaluate(async () => {        
-        var pointStr = document.querySelectorAll("#lb_danhgia")[0].innerText;
-
-        for(let c of pointStr){
-          console.log(c);
-        }
-        
-        ax = [];
-        ii = 0;
-        for (let i of sex) {
-          // console.log(i.innerText);
-          ax.push("SEXN "+i.innerText);
-        }        
-        return new Promise(resolve => {
-          resolve(ax);
+      try {
+        await page.goto(url);
+        // await page.screenshot({ path: 'images/name_'+x+'.png' });
+        const ar = await page.evaluate(async () => {
+          var pointStr = document.querySelectorAll("#lb_danhgia")[0].innerText;
+          var floatPoint = parseFloat(pointStr);
+          // console.log(pointStr);
+          ax = [];
+          ax.push(pointStr);
+          return new Promise(resolve => {
+            resolve(ax);
+          });
         });
-      });
 
-      const vv = await ar;
+        const vv = await ar;
 
-      for(let v of vv){
-        console.log(v);
-        logger.info(v);
-      }
-      p++;
-      if(vv.length == 0){
-        break;
-      }
-            
-      }catch(e){
+        for (let v of vv) {
+          console.log(v);
+          logger.info(v);
+        }
+        p++;
+        if (vv.length != 0) {
+          break;
+        }
+
+      } catch (e) {
         console.log(e);
       }
 
 
     }
 
-    console.log("================================================ END " + x + " ================================================"); 
-    logger.info("================================================ END " + x + " ================================================");
+    // console.log("================================================ END " + x + " ================================================");
+    // logger.info("================================================ END " + x + " ================================================");
   }
- 
+
   browser.close();
-  
+
 })();
