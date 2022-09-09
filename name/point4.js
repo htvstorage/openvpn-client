08f64ad707297be5d1ffb19c127b87c0d2c17b65
tam888888 <trinhvan.tam.group.8@gmail.com>
2022-09-09 09:38:18 +0000

    add

--- name/point4.js
+++ name/point4.js
@@ -21,42 +21,53 @@ log4js.configure({
   // page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
   var myArgs = process.argv.slice(2);
   console.log(myArgs);
+  var dem = [];
   var names = [];
-  for (let f of myArgs) {
-    const data = fs.readFileSync(f, { encoding: 'utf8', flag: 'r' });
-    var nn = data.split("\n");
-    for (let n of nn) names.push(n);
-  }
-//Trịnh Trúc Nhã Phương
+  // for (let f of myArgs) {
+  var data = fs.readFileSync(myArgs[0], { encoding: 'utf8', flag: 'r' });
+  var nn = data.split("\n");
+  for (let n of nn) dem.push(n.trim());
+  // }
+
+  data = fs.readFileSync(myArgs[1], { encoding: 'utf8', flag: 'r' });
+  var nn = data.split("\n");
+  for (let n of nn) names.push(n.trim());
+
+  //Trịnh Trúc Nhã Phương
   var urlp1 = "https://tenchocon.vn/name/Tr%E1%BB%8Bnh-";
-  var urlp2 = "-Nhã-An.html";
+  // var urlp2 = "-Nhã-An.html";
+  var urlp2 = ".html";
 
   for (let x of names) {
-    console.log("====================== Name " + x + " ======================");
-    x = x.replaceAll("\"", "");
-    var p = 0;
-    while (true) {
-      var url = urlp1 + x + urlp2;
-      console.log(url);
-      try {
-        const t = await fetch(url, { timeout: 5000 });
-        var z = await t.text();
-        var key = "lb_danhgia\">";
-        var idx1 = z.indexOf(key) + key.length;
-        var idx2 = z.indexOf("</span>", idx1);
-        var pointStr = z.substring(idx1, idx2);
-        var idx = pointStr.indexOf("/");
-        for (var i = idx; i >= 0; i--) {
-          if (pointStr.charAt(i) == ' ') {
-            console.log(pointStr.substring(i + 1, idx) + " " + pointStr.substring(0, i));
-            logger.info(pointStr.substring(i + 1, idx) + " " + pointStr.substring(0, i));
+    for (let d1 of dem) {
+      for (let d2 of dem) {
+        console.log("====================== Name " + x + " ======================");
+        x = x.replaceAll("\"", "");
+        var p = 0;
+        while (true) {
+          var url = urlp1 + d1 + "-" + d2 + "-" + x + urlp2;
+          console.log(url);
+          try {
+            const t = await fetch(url, { timeout: 5000 });
+            var z = await t.text();
+            var key = "lb_danhgia\">";
+            var idx1 = z.indexOf(key) + key.length;
+            var idx2 = z.indexOf("</span>", idx1);
+            var pointStr = z.substring(idx1, idx2);
+            var idx = pointStr.indexOf("/");
+            for (var i = idx; i >= 0; i--) {
+              if (pointStr.charAt(i) == ' ') {
+                console.log(pointStr.substring(i + 1, idx) + " " + pointStr.substring(0, i));
+                logger.info(pointStr.substring(i + 1, idx) + " " + pointStr.substring(0, i));
+                break;
+              }
+            }
+            // console.log(z.substring(idx1,idx2));
             break;
+          } catch (e) {
+            console.log(e);
           }
         }
-        // console.log(z.substring(idx1,idx2));
-        break;
-      } catch (e) {
-        console.log(e);
       }
     }
   }
