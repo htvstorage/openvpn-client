53a02f3c02340cec5e58bb32fa4b65ef3ac58205
tam888888 <trinhvan.tam.group.8@gmail.com>
2022-12-09 06:57:57 +0000

    add

--- name/indicator.js
+++ name/indicator.js
@@ -1,9 +1,9 @@
 import fetch from "node-fetch";
 import fs from "fs";
 import log4js from "log4js";
-import {Parser} from "json2csv"
+import { Parser } from "json2csv"
 var logger = log4js.getLogger();
-import {SMA,EMA} from 'technicalindicators';
+import { SMA, EMA } from 'technicalindicators';
 import IchimokuCloud from 'technicalindicators'
 log4js.configure({
   appenders: {
@@ -44,56 +44,51 @@ log4js.configure({
   // }
 })();
 
-async function loadData(path){
+async function loadData(path) {
 
   var data = fs.readFileSync(path)
     .toString()
     .split('\n')
-    .map(e => e.trim()) 
-    .map(e => e.split(',').map(e => e.trim())); 
+    .map(e => e.trim())
+    .map(e => e.split(',').map(e => e.trim()));
   let head = data[0];
   data = data.slice(1);
-  data = data.map(e=>{ let x = {}; 
-    for(let i=0; i< head.length;i++){
-      x[head[i].replaceAll("\"","")] = e[i];
+  data = data.map(e => {
+    let x = {};
+    for (let i = 0; i < head.length; i++) {
+      x[head[i].replaceAll("\"", "")] = e[i];
     }
     return x;
   })
-  console.log(data);
+  if (logger.isDebugEnabled)
+    logger.debug(data);
 
-  SMA.calculate({period : 5, values : [1,2,3,4,5,6,7,8,9]});
-  // const SMA = require('technicalindicators').SMA;
-  // var prices = [1,2,3,4,5,6,7,8,9,10,12,13,15];
-  // var period = 10;
-  // SMA.calculate({period : period, values : prices})
-  var prices = data.map(e=> +e.priceClose);
-  var high = data.map(e=> +e.priceHigh);
-  var low = data.map(e=> +e.priceLow);
+  var prices = data.map(e => +e.priceClose);
+  var high = data.map(e => +e.priceHigh);
+  var low = data.map(e => +e.priceLow);
   console.log(prices);
-  let sma = SMA.calculate({period : 5, values : prices});
-  let ema =EMA.calculate({period : 5, values : prices});
-  console.log(sma);
-  console.log(ema);
+
+  let periods = [5, 9, 20, 50, 100, 200];
+
+  let smaRet = periods.map(e => { return SMA.calculate({ period: e, values: prices }); });
+  let emaRet = periods.map(e => { return EMA.calculate({ period: e, values: prices }); });
+
+  console.log(smaRet.length);
+  console.log(emaRet.length);
+
   var ichimokuInput = {
-    high  : high,
-    low   : low,
+    high: high,
+    low: low,
     conversionPeriod: 9,
     basePeriod: 26,
     spanPeriod: 52,
     displacement: 26
   }
-  // input.high = high;
-  // input.low = low;
-  var ichimoku =  IchimokuCloud.ichimokucloud(ichimokuInput)
-  var results = []
-  // ichimokuInput.high.forEach((high, index) => {
-  //   var result = ichimoku.nextValue({ high: high, low: ichimokuInput.low[index]})
-  //   if (result)
-  //     results.push(result)
-  // })  
 
-  console.log(ichimoku);
-  
+  var ichimoku = IchimokuCloud.ichimokucloud(ichimokuInput)
+  if(logger.isDebugEnabled)
+    logger.debug(ichimoku);
+
 }
 async function getTrans(symbol) {
   let a = await fetch("https://api-finance-t19.24hmoney.vn/v1/web/stock/transaction-list-ssi?device_id=web&device_name=INVALID&device_model=Windows+10&network_carrier=INVALID&connection_type=INVALID&os=Chrome&os_version=92.0.4515.131&app_version=INVALID&access_token=INVALID&push_token=INVALID&locale=vi&browser_id=web16693664wxvsjkxelc6e8oe325025&symbol=" + symbol + "&page=1&per_page=2000000", {
