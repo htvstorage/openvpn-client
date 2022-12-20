abfe44d20b15ac03187e3c84bdbcec931270d566
tam888888 <trinhvan.tam.group.8@gmail.com>
2022-12-20 11:31:09 +0000

    add

--- name/Exchange.js
+++ name/Exchange.js
@@ -150,8 +150,9 @@ Exchange.stocks = async function () {
 
 
 
+//Fireant
 
-Exchange.fundamental = async function(stock_code) {
+Exchange.fundamental = async function (stock_code) {
 
   let a = fetch("https://restv2.fireant.vn/symbols/" + stock_code + "/fundamental", {
     "headers": {
@@ -171,4 +172,24 @@ Exchange.fundamental = async function(stock_code) {
   });
 
   return a;
+}
+
+Exchange.financialIndicators = async function (symbol) {
+  return fetch("https://restv2.fireant.vn/symbols/" + symbol + "/financial-indicators", {
+    "headers": {
+      "accept": "application/json, text/plain, */*",
+      "accept-language": "en-US,en;q=0.9,vi-VN;q=0.8,vi;q=0.7",
+      "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkdYdExONzViZlZQakdvNERWdjV4QkRITHpnSSIsImtpZCI6IkdYdExONzViZlZQakdvNERWdjV4QkRITHpnSSJ9.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmZpcmVhbnQudm4iLCJhdWQiOiJodHRwczovL2FjY291bnRzLmZpcmVhbnQudm4vcmVzb3VyY2VzIiwiZXhwIjoxOTQ3MjQ3NzkxLCJuYmYiOjE2NDcyNDc3OTEsImNsaWVudF9pZCI6ImZpcmVhbnQudHJhZGVzdGF0aW9uIiwic2NvcGUiOlsib3BlbmlkIiwicHJvZmlsZSIsInJvbGVzIiwiZW1haWwiLCJhY2NvdW50cy1yZWFkIiwiYWNjb3VudHMtd3JpdGUiLCJvcmRlcnMtcmVhZCIsIm9yZGVycy13cml0ZSIsImNvbXBhbmllcy1yZWFkIiwiaW5kaXZpZHVhbHMtcmVhZCIsImZpbmFuY2UtcmVhZCIsInBvc3RzLXdyaXRlIiwicG9zdHMtcmVhZCIsInN5bWJvbHMtcmVhZCIsInVzZXItZGF0YS1yZWFkIiwidXNlci1kYXRhLXdyaXRlIiwidXNlcnMtcmVhZCIsInNlYXJjaCIsImFjYWRlbXktcmVhZCIsImFjYWRlbXktd3JpdGUiLCJibG9nLXJlYWQiLCJpbnZlc3RvcGVkaWEtcmVhZCJdLCJzdWIiOiIxZDY5YmE3NC0xNTA1LTRkNTktOTA0Mi00YWNmYjRiODA3YzQiLCJhdXRoX3RpbWUiOjE2NDcyNDc3OTEsImlkcCI6Ikdvb2dsZSIsIm5hbWUiOiJ0cmluaHZhbmh1bmdAZ21haWwuY29tIiwic2VjdXJpdHlfc3RhbXAiOiI5NTMyOGNlZi1jZmY1LTQ3Y2YtYTRkNy1kZGFjYWJmZjRhNzkiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJ0cmluaHZhbmh1bmdAZ21haWwuY29tIiwidXNlcm5hbWUiOiJ0cmluaHZhbmh1bmdAZ21haWwuY29tIiwiZnVsbF9uYW1lIjoiVHJpbmggVmFuIEh1bmciLCJlbWFpbCI6InRyaW5odmFuaHVuZ0BnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6InRydWUiLCJqdGkiOiJhMTY2MDQwOGNhMGFkYWQxOTcwZDVhNWZhMmFjNjM1NSIsImFtciI6WyJleHRlcm5hbCJdfQ.cpc3almBHrGu-c-sQ72hq6rdwOiWB1dIy1LfZ6cgjyH4YaBWiQkPt4l7M_nTlJnVOdFt9lM2OuSmCcTJMcAKLd4UmdBypeZUpTZp_bUv1Sd3xV8LHF7FSj2Awgw0HIaic08h1LaRg0pPzzf-IRJFT7YA8Leuceid6rD4BCQ3yNvz8r58u2jlCXuPGI-xA8W4Y3151hpNWCtemyizhzi7EKri_4WWpXrXPAeTAnZSdoSq87shTxm9Kyz_QJUBQN6PIEINl9sIQaKL-I_jR9LogYB_aM3hs81Ga6h-n-vbnFK8JR1JEJQmU-rxyX7XvuL-UjQVag3LxQeJwH7Nnajkkg",
+      "sec-ch-ua": "\"Chromium\";v=\"92\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"92\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-site"
+    },
+    "referrerPolicy": "no-referrer",
+    "body": null,
+    "method": "GET",
+    "mode": "cors",
+    "credentials": "include"
+  });
 }
--- name/test2.js
+++ name/test2.js
@@ -1,20 +1,64 @@
 
 import { Exchange } from "./Exchange.js";
-(async () =>{
+import fetch from "node-fetch";
+import fs from "fs";
+
+
+(async () => {
     console.log("ABC")
     let z = await Exchange.ratios("HPG");
-    let x = [1,2,3,4,5,6,7,8,9]
+    let x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     console.log(await z.text())
     console.log(x.slice(-3))
     console.log(x)
 
     let zzzz = "123456789";
 
-    console.log(zzzz.substring(-3,3));
+    console.log(zzzz.substring(-3, 3));
 
-    let ex = (path)=>{
-        return path.includes("HOSE")?"HOSE":path.includes("HNX")?"HNX":"UPCOM";
+    let ex = (path) => {
+        return path.includes("HOSE") ? "HOSE" : path.includes("HNX") ? "HNX" : "UPCOM";
     }
 
     console.log(ex("ABCUPCOMXXXX"));
+
+    let z2 = await Exchange.stocks();
+
+    // console.log(await z2.json())
+
+    let data = fs.readFileSync('stock.json');
+    let stockData = JSON.parse(data);
+
+    let a = [];
+    let b = [];
+    for (let e of Object.keys(stockData)) {
+        // console.log(stockData[e]);
+        // var t = {};
+        // t[e] = stockData[e].fundamental;
+        stockData[e].fundamental["symbol"] = e;
+        stockData[e].financialIndicators["symbol"] = e;
+        if(stockData[e].financialIndicators != undefined){
+            a.push(stockData[e].financialIndicators);
+        }else{
+            console.log("a.push(stockData[e].financialIndicators);",e)
+        }
+        
+        b.push([stockData[e].fundamental,stockData[e].financialIndicators])
+    }
+
+    console.log(a.length)
+    a = a.sort((x, y) => {
+        console.log(x["symbol"])
+        let t = x[2]["value"] - (y[2]["value"]);
+        return t > 0? 1 : t < 0 ? -1 : 0
+    });
+
+    
+
+    for (var i = 0; i < a.length; i++) {
+        console.log(a[i]["symbol"],a[i][2]["value"]);
+    }
+
+    console.log(b[0]);
+
 })();
