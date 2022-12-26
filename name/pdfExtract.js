788cc43d1dc2c6bdc45342d708940fb9d65baf5e
tam888888 <trinhvan.tam.group.8@gmail.com>
2022-12-26 08:15:47 +0000

    add

--- name/pdfExtract.js
+++ name/pdfExtract.js
@@ -20,7 +20,7 @@ function url(date) {
     let dir = "./pdf";
     let req = 0;
     let res = 0;
-    for (var i = 400; i < 1000; i++) {
+    for (var i = 1; i < 3; i++) {
         let x = url(new Date(Date.now() - i * 24 * 60 * 60 * 1000));
         const response = fetch(x.url);
         req++;
