1fee935f3e83f58aa674bc21f7009280c8ad1dc0
tam888888 <trinhvan.tam.group.8@gmail.com>
2023-02-01 09:08:39 +0000

    add

--- name/stat.js
+++ name/stat.js
@@ -8,18 +8,37 @@ import { Console } from 'node:console'
 import { Transform } from 'node:stream'
 import fs from "fs";
 import { Parser } from "json2csv"
+import path from "path";
+
+function getNow() {
+    let fd = new Date();
+    return fd.getFullYear()
+        + "" + (fd.getMonth() + 1 < 10 ? "0" + (fd.getMonth() + 1) : fd.getMonth() + 1)
+        + "" + (fd.getDate() < 10 ? "0" + fd.getDate() : fd.getDate());
+}
 
 (async () => {
 
-    let dto = Date.now() / 1000
-    let dfrom = new Date(Date.now - 30 * 24 * 60 * 60);
-    let symbols = ['HPG', 'PLC', 'SHS', 'VIX', 'KSB']
+    let dto = Date.now() / 1000 + 7 * 60 * 60
+    let dfrom = new Date(dto - 1 * 24 * 60 * 60);
+    let symbols = ['HPG', 'PLC', 'SHS', 'VIX', 'KSB', 'VN30', 'VNINDEX']
     const ts = new Transform({ transform(chunk, enc, cb) { cb(null, chunk) } })
     const logger = new Console({ stdout: ts })
 
     function getTable(data) {
         logger.table(data)
         return (ts.read() || '').toString()
+    }
+
+
+    let dir = "./stattest/" + getNow() + "/";
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    } else {
+        let files = fs.readdirSync(dir);
+        for (const file of files) {
+            fs.unlinkSync(path.join(dir, file));
+        }
     }
 
     for (let symbol of symbols) {
@@ -42,24 +61,30 @@ import { Parser } from "json2csv"
 
 
         let z = await a.json()
+        if (z.o == undefined) continue;
 
         let z1 = z.o.map((e, i) => { return { c: z.c[i], h: z.h[i], l: z.l[i], o: z.o[i], t: z.t[i], v: z.v[i] } })
 
         let z2 = z1.reduce((a, b) => { return { v: (a.v + b.v) } }, { v: 0 })
 
         let av = z2.v / z1.length
-        let z3 = z1.map(e => { e['av'] = av; e['r'] = (e.v / av).toFixed(2); e.date = (new Date(e.t * 1000 + 7*60*60*1000)).toISOString(); return e })
+        let z3 = z1.map(e => {
+            e['av'] = av; e['r'] = (e.v / av).toFixed(2);
+            e.date = (new Date(e.t * 1000 + 7 * 60 * 60 * 1000)).toISOString();
+            e.updown = (e.c > e.o) ? "up" : (e.c < e.o) ? "down" : "-";
+            return e;
+        })
 
 
 
         const str = getTable(z3)
         console.log(str.length) // 105
         console.log(str)
-        fs.writeFile("./stat/" + symbol + "_report_5phut_table.txt", str, (e) => { })
-        let csv = new Parser({ fields: Object.keys[z3[0]]});
+        fs.writeFile(dir + symbol + "_report_5phut_table.txt", str, (e) => { })
+        let csv = new Parser({ fields: Object.keys[z3[0]] });
         let data2 = csv.parse(z3);
-        fs.writeFile("./stat/" + symbol + "_5phut_table.json", JSON.stringify(z3), (e) => { })
-        fs.writeFile("./stat/" + symbol + "_5phut_table.csv", data2, (e) => { })
+        fs.writeFile(dir + symbol + "_5phut_table.json", JSON.stringify(z3), (e) => { })
+        fs.writeFile(dir + symbol + "_5phut_table.csv", data2, (e) => { })
     }
 
 
--- name/statall.js
+++ name/statall.js
@@ -8,16 +8,25 @@ import { Console } from 'node:console'
 import { Transform } from 'node:stream'
 import fs from "fs";
 import { Parser } from "json2csv"
-import { stat } from "node:fs";
+import path from "path";
 
 const httpAgent = new http.Agent({ keepAlive: true });
 const httpsAgent = new https.Agent({ keepAlive: true });
 const agent = (_parsedURL) => _parsedURL.protocol == 'http:' ? httpAgent : httpsAgent;
 
+
+function getNow() {
+    let fd = new Date();
+    return fd.getFullYear()
+        + "" + (fd.getMonth() + 1 < 10 ? "0" + (fd.getMonth() + 1) : fd.getMonth() + 1)
+        + "" + (fd.getDate() < 10 ? "0" + fd.getDate() : fd.getDate());
+}
+
+
 (async () => {
 
-    let dto = Date.now() / 1000
-    let dfrom = new Date(Date.now - 30 * 24 * 60 * 60);
+    let dto = Date.now() / 1000 + 7 * 60 * 60
+    let dfrom = new Date(dto - 1 * 24 * 60 * 60);
     let s = await Exchange.vndGetAllSymbols();
     // console.table(sym)
     s = s.map(e => e.code).filter(e => { return e.length == 3 });
@@ -33,6 +42,21 @@ const agent = (_parsedURL) => _parsedURL.protocol == 'http:' ? httpAgent : https
     let stat = { req: 0, res: 0 }
 
     console.log(symbols)
+    symbols.push('VNINDEX')
+    symbols.push('VN30')
+    symbols.push('HNX')
+    symbols.push('HNX30')
+    symbols.push('UPCOM')
+
+    let dir = "./stat/" + getNow() + "/";
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    } else {
+      let files = fs.readdirSync(dir);
+      for (const file of files) {
+        fs.unlinkSync(path.join(dir, file));
+      }
+    }
 
     for (let symbol of symbols) {
         console.log(symbol)
@@ -58,7 +82,7 @@ const agent = (_parsedURL) => _parsedURL.protocol == 'http:' ? httpAgent : https
         a.then(res => res.json()).then(data => {
             // console.log(data)
             let z = data;
-            stat.res ++;
+            stat.res++;
             if (z.o.length == 0) {
                 return;
             }
@@ -67,21 +91,27 @@ const agent = (_parsedURL) => _parsedURL.protocol == 'http:' ? httpAgent : https
             let z2 = z1.reduce((a, b) => { return { v: (a.v + b.v) } }, { v: 0 })
 
             let av = z2.v / z1.length
-            let z3 = z1.map(e => { e['av'] = av; e['r'] = (e.v / av).toFixed(2); e.date = (new Date(e.t * 1000 + 7 * 60 * 60 * 1000)).toISOString(); return e })
+            let z3 = z1.map(e => {
+                e['av'] = av; e['r'] = (e.v / av).toFixed(2);
+                e.date = (new Date(e.t * 1000 + 7 * 60 * 60 * 1000)).toISOString();
+                e.updown = (e.c > e.o) ? "up" : (e.c < e.o) ? "down" : "-";
+                return e
+            })
 
+            
 
 
             const str = getTable(z3)
             console.log(str.length) // 105
             console.log(str)
-            fs.writeFile("./stat/" + symbol + "_report_5phut_table.txt", str, (e) => { })
+            fs.writeFile(dir + symbol + "_report_5phut_table.txt", str, (e) => { })
             let csv = new Parser({ fields: Object.keys[z3[0]] });
             let data2 = csv.parse(z3);
-            fs.writeFile("./stat/" + symbol + "_5phut_table.json", JSON.stringify(z3), (e) => { })
-            fs.writeFile("./stat/" + symbol + "_5phut_table.csv", data2, (e) => { })
+            fs.writeFile(dir + + symbol + "_5phut_table.json", JSON.stringify(z3), (e) => { })
+            fs.writeFile(dir + symbol + "_5phut_table.csv", data2, (e) => { })
         })
 
-        while(stat.req - stat.res >= 20){
+        while (stat.req - stat.res >= 50) {
             await Exchange.wait(100)
         }
         console.log(stat)
