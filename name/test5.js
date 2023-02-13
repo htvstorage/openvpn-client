d5e0a6a56eda2323ab5464cc5c78a65272b39988
tam888888 <trinhvan.tam.group.8@gmail.com>
2023-02-13 08:43:58 +0000

    ok

--- name/test5.js
+++ name/test5.js
@@ -10,74 +10,58 @@ import fs from "fs";
 import xlsx from "xlsx"
 
 (async () => {
-    let p = '1.2'
-    console.log(+p,p)
-
-    let Headers = ['ChangeId', 'ChangeDescription', 'ChangeDate', 'Enhancement/Fix', 'ExcutorTeam'];
-    let Data = ['INC1234', 'Multiple Cert cleanup', '04/07/2022', 'Enhancement', 'IlevelSupport'];
-    
-    let workbook = xlsx.utils.book_new();
-
-    
-    // xlsx.utils.sheet_add_aoa(worksheet, [Headers], { origin: 'A1' });
-    // xlsx.utils.sheet_add_aoa(worksheet, [Data], { origin: 'A2' });
 
 
-   let a = await Exchange.vndGetAllSymbols();
-   console.log(a)
-    
-    
-   let worksheet = xlsx.utils.json_to_sheet(a);
-    
-   xlsx.utils.book_append_sheet(workbook, worksheet);
-    xlsx.writeFile(workbook, "Test.xlsx");
-    console.log("written")
 
 
-  let code="VNINDEX";
-    
-  let start = 1421028900;
-  let end = Math.floor(Date.now() / 1000);
-  let out = { t: [], v: [], o: [], c: [], h: [], l: [] };
-  while (true) {
-    let a = await fetch("https://chartdata1.mbs.com.vn/pbRltCharts/chart/v2/history?symbol=" + code + "&resolution="+"1"+"&from=" + start + "&to=" + end, {
-      "headers": {
-        "accept": "*/*",
-        "accept-language": "en-US,en;q=0.9,vi-VN;q=0.8,vi;q=0.7",
-        "content-type": "text/plain",
-        "sec-ch-ua": "\"Chromium\";v=\"92\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"92\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-site"
-      },
-      "referrer": "https://sweb.mbs.com.vn/",
-      "referrerPolicy": "strict-origin-when-cross-origin",
-      "body": null,
-      "method": "GET",
-      "mode": "cors",
-      agent
-    });
-    let z = await a.json();
-    if (z.t.length == 0) {
-      break;
-    } else if (z.t.at(-1) == start) {
-      out.t.push(...z.t);
-      out.v.push(...z.v);
-      out.o.push(...z.o);
-      out.c.push(...z.c);
-      out.h.push(...z.h);
-      out.l.push(...z.l);
-      break;
-    } else {
-      out.t.push(...z.t);
-      out.v.push(...z.v);
-      out.o.push(...z.o);
-      out.c.push(...z.c);
-      out.h.push(...z.h);
-      out.l.push(...z.l);
-      start = z.t.at(-1);
-    }
-  }
+  let Headers = ['ChangeId', 'ChangeDescription', 'ChangeDate', 'Enhancement/Fix', 'ExcutorTeam'];
+  let Data = ['INC1234', 'Multiple Cert cleanup', '04/07/2022', 'Enhancement', 'IlevelSupport'];
 
+  let workbook = xlsx.utils.book_new();
+
+
+  // xlsx.utils.sheet_add_aoa(worksheet, [Headers], { origin: 'A1' });
+  // xlsx.utils.sheet_add_aoa(worksheet, [Data], { origin: 'A2' });
+
+
+  let a = await Exchange.vndGetAllSymbols();
+  console.log(a)
+
+
+  let worksheet = xlsx.utils.json_to_sheet(a);
+
+  xlsx.utils.book_append_sheet(workbook, worksheet);
+  xlsx.writeFile(workbook, "Test.xlsx");
+  console.log("written")
+
+
+  let kq=await fetch("https://finance.vietstock.vn/data/allkqkdorder", {
+    "headers": {
+      "accept": "*/*",
+      "accept-language": "en-US,en;q=0.9,vi-VN;q=0.8,vi;q=0.7",
+      "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+      "sec-ch-ua": "\"Chromium\";v=\"92\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"92\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-requested-with": "XMLHttpRequest",
+      "cookie": "_cc_id=bd4b49a4b7a58cfaeb38724516b82171; dable_uid=35370669.1668475569175; dable_uid=35370669.1668475569175; __gads=ID=af0897d5e697b47b-2219cdf973d800fc:T=1668475507:S=ALNI_MaL0TTHW6nK5yq36h7SKojzDZdS3w; language=vi-VN; Theme=Light; isShowLogin=true; ASP.NET_SessionId=qzmb5wr2wd0sopqbgkmen5k1; __RequestVerificationToken=FcdJ2mHUqoXKgPnjcsW5RMkgHzYwGAN1tqm9F3oX4KwcZwT1JSg1BaOPKdzi8cNhluuOKvd-bKPR5EkZclt-BSiApZ3DYQWiNZ4wS6bPc1g1; AnonymousNotification=; _gid=GA1.2.1513324323.1676275818; _gat_UA-1460625-2=1; __gpi=UID=00000b7c20f7c81e:T=1668475507:RT=1676275827:S=ALNI_MbZNgtWTtbRI1MrLcfM5qFq0RIBMQ; panoramaId_expiry=1676362228300; panoramaId=4d0c5a1ada413b921c9b37c6d8d4a9fb927a0d4a71477931e7d9d438d60721a2; cto_bundle=MZdXjF9GalVXbDclMkJNeHNwS0pTN0VtbThsUDhJUmZPcHZIWFhkOGljTGl1dEV5Wm0lMkZ1eXZFaENDWGtzWnRKUDdUWTVzb2d1JTJGQTVnNjB4enZtaTB0dHpxQTZKc2taMDJOTEg2eSUyQmc4UVFSRDlwJTJCQU1DMDJuRUIwJTJGRVBGaSUyRjgxOGxEcXhSeHN1enUxNjFwYTNtR0M3blkySFpFQSUzRCUzRA; vts_usr_lg=312E3E6B21B988136F5F94F676DA2BF0BAC0FB3C7A9177453BBAE2736CC98DFC6700E661F85D549337E3B305F1411E26400C934C13702CDDE599D35A2CD2CFA91707BD3F641F84DF47259462B00A15D2395CE003E296705C4DF38FB1A2B35A9D98106FF72953941CC17840362F45AF251FC1A9B776A73BBEF69E9F568348C2F0; vst_usr_lg_token=EOEnV6giRkGbsPZqfVJs8Q==; _ga_EXMM0DKVEX=GS1.1.1676275817.1.1.1676275856.0.0.0; _ga=GA1.2.70802173.1668475499"
+    },
+    "referrer": "https://finance.vietstock.vn/ket-qua-kinh-doanh",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": "catID=0&industryID=0&code=&order=&__RequestVerificationToken=1fQIVecNb69fQDtMul9VM37hH02DE9WGec9sRvfZAPUoCmxxywnna_EcAxSjSVZ8do3T8gtkfuqxj4N2vM4izYvhUxdMlxnlXOVtUaBAbPHCkGiCoVDB6GjFr1tbT-640",
+    "method": "POST",
+    "mode": "cors"
+  });
+
+  let z = await kq.json();
+
+  console.table(z)
+  workbook = xlsx.utils.book_new();
+  worksheet = xlsx.utils.json_to_sheet(z);
+
+  xlsx.utils.book_append_sheet(workbook, worksheet);
+  xlsx.writeFile(workbook, "KQKD.xlsx");
+  console.log("written")
 })();
