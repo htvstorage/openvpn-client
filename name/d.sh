b30cbb860559e75ed1c7fb082e7525681deb5d3f
tam888888 h <trinhvan.tam.group.8@gmail.com>
2023-04-05 16:13:51 +0000

    add

---
+++ name/d.sh
@@ -0,0 +1,6 @@
+nohup node  script/dumpdaily2.js stock=24HMONEY >> dum.log 2>&1 < /dev/null &
+nohup node  script/dumpdaily2.js stock=VIETSTOCK >> dum.log 2>&1 < /dev/null &
+nohup node  script/dumpdaily2.js stock=SSI >> dum.log 2>&1 < /dev/null &
+nohup node  script/dumpdaily2.js stock=TCBS >> dum.log 2>&1 < /dev/null &
+nohup node  script/dumpdaily2.js stock=VCI >> dum.log 2>&1 < /dev/null &
+nohup node  script/dumpdaily2.js stock=VCBS >> dum.log 2>&1 < /dev/null &
