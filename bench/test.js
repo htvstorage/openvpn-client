0972be47bcd46d6215320d8674d424901c8b39b7
tam888888 h <trinhvan.tam.group.8@gmail.com>
2023-07-26 04:23:50 +0000

    add

--- bench/test.js
+++ bench/test.js
@@ -1,4 +1,6 @@
 const MetricsBenchmark = require('./index.js');
+const { promisify } = require('util');
+const sleepNanos = promisify(setImmediate);
 
 // Initialize the MetricsBenchmark instance
 const metrics = MetricsBenchmark.getInstance();
@@ -10,13 +12,23 @@ function processMessage() {
   const messageSize = 100; // Replace with the actual size of the message
   // console.log(incomingTime)
   // Call the statisticMetris method to measure performance
+  
   metrics.statisticMetris(incomingTime, messageSize, 'My Object');
 }
 
+
+let maxtps = 10000;
+const start = Date.now();
 // Simulate processing messages
+(async () =>{
 for (let i = 0; i < 10000000000; i++) {
+  let end = Date.now();
+  while(i*1000/(end - start) >= maxtps){
+    await sleepNanos(10)
+    end = Date.now();
+  }
   processMessage();
 }
-
+})()
 const incomingTime = process.hrtime.bigint()
 console.log(incomingTime)
