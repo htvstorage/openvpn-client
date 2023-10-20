f523f7eb1f3d684414c7d430fd7e5e3eadc8f69b
tam888888 h <trinhvan.tam.group.8@gmail.com>
2023-10-20 11:00:11 +0000

    add

--- name/websocket/compress.sh
+++ name/websocket/compress.sh
@@ -3,7 +3,7 @@ sudo apt install 7zip;
 d=`date +'%Y%m%d'`
 7zz a data$d".7z" data$d".txt"
 cd /workspace/openvpn-client/name/websocket
-git add "./data"$d".7z"
+git add data$d.7z
 git commit -m "add"
 git push
 
