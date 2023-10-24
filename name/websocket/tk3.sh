a09680932a8a32971d942f129b632f1ac991139b
tam888888 h <trinhvan.tam.group.8@gmail.com>
2023-10-24 04:33:50 +0000

    add

--- name/websocket/tk3.sh
+++ name/websocket/tk3.sh
@@ -9,6 +9,7 @@ fi
 input_file="$1"
 record="$2"
 interval="$3"
+SYMBOLS="$4"
 if [ -z "$record" ]; then
     # Nếu biến chưa được khởi tạo, gán giá trị mặc định là 10
     record=10
@@ -17,6 +18,10 @@ fi
 if [ -z "$interval" ]; then
     # Nếu biến chưa được khởi tạo, gán giá trị mặc định là 10
     interval=5
+fi
+if [ -z "$SYMBOLS" ]; then
+    # Nếu biến chưa được khởi tạo, gán giá trị mặc định là 10
+    SYMBOLS="HPG-SSI-DIG-PDR-DXG-VCI-HSG-NKG-DGW"
 fi
 
 # Khởi tạo biến lưu trữ các thông tin theo mã chứng khoán và thời gian
@@ -28,7 +33,7 @@ declare -A sell_value
 declare -A unknown_value
 
 # Sử dụng awk để duyệt qua các dòng và tính toán
-awk -F"|" -v record="$record" -v interval="$interval" 'BEGIN{
+awk -F"|" -v record="$record" -v interval="$interval" -v MA="$SYMBOLS" 'BEGIN{
     ss="AAA-AAM-AAT-ABR-ABS-ABT-ACB-ACC-ACG-ACL-ADG-ADP-ADS-AGG-AGM-AGR-ANV-APC-APG-APH-ASG-ASM-ASP-AST-BAF-BBC-BCE-BCG-BCM-BFC-BHN-BIC-BID-BKG-BMC-BMI-BMP-BRC-BSI-BTP-BTT-BVH-BWE-C32-C47-CAV-CCI-CCL-CDC-CHP-CIG-CII-CKG-CLC-CLL-CLW-CMG-CMV-CMX-CNG-COM-CRC-CRE-CSM-CSV-CTD-CTF-CTG-CTI-CTR-CTS-CVT-D2D-DAG-DAH-DAT-DBC-DBD-DBT-DC4-DCL-DCM-DGC-DGW-DHA-DHC-DHG-DHM-DIG-DLG-DMC-DPG-DPM-DPR-DQC-DRC-DRH-DRL-DSN-DTA-DTL-DTT-DVP-DXG-DXS-DXV-EIB-ELC-EVE-EVF-EVG-FCM-FCN-FDC-FIR-FIT-FMC-FPT-FRT-FTS-GAS-GDT-GEG-GEX-GIL-GMC-GMD-GMH-GSP-GTA-GVR-HAG-HAH-HAP-HAR-HAS-HAX-HBC-HCD-HCM-HDB-HDC-HDG-HHP-HHS-HHV-HID-HII-HMC-HNG-HPG-HPX-HQC-HRC-HSG-HSL-HT1-HTI-HTL-HTN-HTV-HU1-HUB-HVH-HVN-HVX-IBC-ICT-IDI-IJC-ILB-IMP-ITA-ITC-ITD-JVC-KBC-KDC-KDH-KHG-KHP-KMR-KOS-KPF-KSB-L10-LAF-LBM-LCG-LDG-LEC-LGC-LGL-LHG-LIX-LM8-LPB-LSS-MBB-MCP-MDG-MHC-MIG-MSB-MSH-MSN-MWG-NAF-NAV-NBB-NCT-NHA-NHH-NHT-NKG-NLG-NNC-NO1-NSC-NT2-NTL-NVL-NVT-OCB-OGC-OPC-ORS-PAC-PAN-PC1-PDN-PDR-PET-PGC-PGD-PGI-PGV-PHC-PHR-PIT-PJT-PLP-PLX-PMG-PNC-PNJ-POM-POW-PPC-PSH-PTB-PTC-PTL-PVD-PVP-PVT-QBS-QCG-RAL-RDP-REE-S4A-SAB-SAM-SAV-SBA-SBT-SBV-SC5-SCD-SCR-SCS-SFC-SFG-SFI-SGN-SGR-SGT-SHA-SHB-SHI-SHP-SIP-SJD-SJF-SJS-SKG-SMA-SMB-SMC-SPM-SRC-SRF-SSB-SSC-SSI-ST8-STB-STG-STK-SVC-SVD-SVI-SVT-SZC-SZL-TBC-TCB-TCD-TCH-TCL-TCM-TCO-TCR-TCT-TDC-TDG-TDH-TDM-TDP-TDW-TEG-TGG-THG-TIP-TIX-TLD-TLG-TLH-TMP-TMS-TMT-TN1-TNA-TNC-TNH-TNI-TNT-TPB-TPC-TRA-TRC-TSC-TTA-TTB-TTE-TTF-TV2-TVB-TVS-TVT-TYA-UIC-VAF-VCA-VCB-VCF-VCG-VCI-VDP-VDS-VFG-VGC-VHC-VHM-VIB-VIC-VID-VIP-VIX-VJC-VMD-VND-VNE-VNG-VNL-VNM-VNS-VOS-VPB-VPD-VPG-VPH-VPI-VPS-VRC-VRE-VSC-VSH-VSI-VTB-VTO-YBM-YEG"
     split(ss,aa, "-")
     m["HPG"]="HPG"
@@ -41,6 +46,12 @@ awk -F"|" -v record="$record" -v interval="$interval" 'BEGIN{
         m30[aa[a]]=aa[a]
     }
     # print m["AAA"]
+
+    split(MA,aa, "-")
+    for(a in aa){
+        ma[aa[a]]=aa[a]
+    }
+
     current_time = systime()+ 7 *60*60;  # Lấy thời gian hiện tại dưới dạng Unix timestamp
     midnight_time = mktime(strftime("%Y %m %d 00 00 00", current_time));  # Lấy thời gian bắt đầu của ngày dưới dạng Unix timestamp
     delta = (current_time - midnight_time)/60
@@ -53,6 +64,7 @@ awk -F"|" -v record="$record" -v interval="$interval" 'BEGIN{
         # print "000" $0  
         symbol = $1
         s2 = substr($1,3)
+        symbol=s2
         # print s2
         time = $5
         action = $7
@@ -125,7 +137,7 @@ awk -F"|" -v record="$record" -v interval="$interval" 'BEGIN{
 }
 END {
     # In tiêu đề
-    printf "%-15s%-15s%-20s%-20s%-15s%-15s%-15s%-15s%-15s%-15s%-15s\n", "Thời gian", "Mã Chứng Khoán", "Mua-Ban", "Tổng" ,"TPS","Mua", "Bán", "Không Xác Định", "Giá Trị Mua", "Giá Trị Bán", "Giá Trị Không Xác Định"
+    printf "%-15s%-15s%-20s%-20s%-20s%-15s%-15s%-15s%-15s%-15s%-15s\n", "Thời gian", "Mã Chứng Khoán", "Mua-Ban", "Tổng" ,"TPS","Mua", "Bán", "Không Xác Định", "Giá Trị Mua", "Giá Trị Bán", "Giá Trị Không Xác Định"
 
     for(symbol in symbols){    
         sum[symbol,0]=0        
@@ -152,7 +164,7 @@ END {
             # print v    
             c++        
             if(c >= l-record) {
-                printf "%02d:%02d-%02d:%02d  %-20s%-20'\''.0f%-20'\''.0f%-20'\''.0f%-15d%-15d%-15d%-15d%-15d%-15d\n", int(time_slot/60), time_slot%60, int((time_slot+interval)/60), (time_slot+interval)%60, symbol, giatrimua-giatriban,t,tps ,mua, ban, khongxacdinh, giatrimua, giatriban, giatrikhongxacdinh
+                printf "%02d:%02d-%02d:%02d    %-15s%-20'\''.0f%-20'\''.0f%-20'\''.0f%-15d%-15d%-15d%-15d%-15d%-15d\n", int(time_slot/60), time_slot%60, int((time_slot+interval)/60), (time_slot+interval)%60, symbol, giatrimua-giatriban,t,tps ,mua, ban, khongxacdinh, giatrimua, giatriban, giatrikhongxacdinh
             }                                                   
             sum[symbol,0] += giatrimua-giatriban            
             sum[symbol,1] += giatrimua+giatriban+giatrikhongxacdinh
@@ -166,10 +178,17 @@ END {
     p[1]="VNINDEX"
     p[2]="ALL"
     p[3]="VN30"
-    printf "%-15s%-15s%-20s%-20s%-15s%-15s%-15s%-15s%-15s%-15s%-15s\n", "Thời gian", "Mã Chứng Khoán", "Mua-Ban", "Tổng" ,"Mua", "Bán","TPS", "Không Xác Định", "Giá Trị Mua", "Giá Trị Bán", "Giá Trị Không Xác Định"
+    # p[4]="HPG"
+    for(s in ma){
+        print ma[s]" "length(p)+1
+        p[length(p)+1] = ma[s]               
+    }
+    printf "%-15s%-15s%-20s%-20s%-20s%-20s%-20s%-20s%\n", "Thời gian", "Mã Chứng Khoán", "Mua-Ban", "Tổng" ,"Mua", "Bán","Mua-Bán","TPS"
+    time=strftime("%H:%M:%S", current_time)
     for(S in p){
-        t=p[S]
-        printf "%-15s%-20'\''.0f%-20'\''.0f%-20'\''.0f%-20'\''.0f%-20'\''.0f%-20'\''.0f\n", t,sum[t,0],sum[t,1],sum[t,2],sum[t,3],sum[t,4],sum[t,5]/sum[t,6]
+        t=p[S]        
+        # printf "%-15s%-15s%-20'\''.0f%-20'\''.0f%-20'\''.0f%-20'\''.0f%-20'\''.0f%-20'\''.0f\n", time,t,sum[t,0],sum[t,1],sum[t,2],sum[t,3],sum[t,4],sum[t,5]/sum[t,6]
+        printf "%-15s%-15s%-20'\''.0f%-20'\''.0f%-20'\''.0f%-20'\''.0f%-20'\''.0f%-20'\''.0f\n", time,t,sum[t,0],sum[t,1],sum[t,2],sum[t,3],sum[t,4],sum[t,5],sum[t,6]
     }
     
     
