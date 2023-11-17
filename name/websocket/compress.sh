6fd522f029fb0b40caa877860bdb57c99c1a320d
tam888888 h <trinhvan.tam.group.8@gmail.com>
2023-11-17 16:18:04 +0000

    add

--- name/websocket/compress.sh
+++ name/websocket/compress.sh
@@ -6,9 +6,13 @@ d=`date +'%Y%m%d'`
 7zz a data_ps_$d".7z" data_ps_$d".txt"
 cd /workspace/openvpn-client/name/websocket
 git add data$d.7z 
+sleep 5
 git add data3$d.7z
+sleep 5
 git add data_ps_$d".7z"
+sleep 5
 git commit -m "add"
+sleep 5
 git push
 
 
