7bdd37bcdf50112c247ecad3ee139713c9970907
tam888888 h <trinhvan.tam.group.8@gmail.com>
2023-12-05 10:42:28 +0000

    add

--- name/webapp2/app.js
+++ name/webapp2/app.js
@@ -48,7 +48,8 @@ for (let i = 1; i <= 10; i++) {
   });
 }
 
-enpoint = ['symbol', 'history', 'detail', 'chart2Row', 'timeline', 'timelinemulti', 'sectors', 'sectorschart', 'stackbar', 'skybox', 'skybox2', 'skyboxok', 'tooltips']
+
+enpoint = ['symbol', 'history', 'detail', 'chart2Row', 'timeline', 'timelinemulti', 'sectors', 'sectorschart', 'stackbar', 'skybox', 'skybox2', 'skyboxok', 'tooltips', 'skyboxX','skyboxX1']
 enpoint.forEach(ep => {
   app.get(`/${ep}`, (req, res) => {
     res.sendFile(__dirname + `/public/${ep}.html`);
